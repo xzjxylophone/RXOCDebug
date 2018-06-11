c7d3a049d456ec62d58cb8d4f7adfc2247afb936
Rush <xzjxylophone@126.com>
2018-06-11 21:15:06 +0800

    fixed

---
+++ RXOCDebugDemo/RXOCDebugDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RXOCDebugDemo
+//
+//  Created by Rush.D.Xzj on 2018/6/11.
+//  Copyright © 2018 TAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RXOCDebugDemo/RXOCDebugDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RXOCDebugDemo
+//
+//  Created by Rush.D.Xzj on 2018/6/11.
+//  Copyright © 2018 TAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
